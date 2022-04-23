9095c1e37e5b6f182dc4ce0cf67952e44870c558
KulAnkur <kulankur5@gmail.com>
2022-04-23 20:52:34 +0530

    Register activity

---
+++ app/src/main/java/com/example/yes2life/Register_main.java
@@ -0,0 +1,14 @@
+package com.example.yes2life;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Register_main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_main);
+    }
+}
